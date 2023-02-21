2d28aa41adbdb969183d2e86d6041fc77b97bb64
Erik Hennerfors <erik.hennerfors@laget.se>
2023-02-21 09:00:40 +0100

    Added ContainerBuilderExtensions.cs file

---
+++ laget.Mapper/Extensions/ContainerBuilderExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Autofac;
+using laget.Mapper.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace laget.Mapper.Extensions
+{
+    public static class ContainerBuilderExtensions
+    {
+        public static void RegisterMaps(this ContainerBuilder builder)
+        {
+            if (builder == null) throw new ArgumentNullException(nameof(builder));
+
+            var assembly = Assembly.GetEntryAssembly();
+            if (assembly == null) throw new ArgumentNullException(nameof(assembly));
+
+            builder.RegisterAssemblyTypes(assembly)
+                .AssignableTo<IMapper>()
+                .AsImplementedInterfaces();
+
+            builder.RegisterBuildCallback(c =>
+            {
+                var logger = c.Resolve<ILogger<Mappings>>();
+                var mappers = c.Resolve<IEnumerable<IMapper>>();
+
+                Mapper.RegisterLogger(logger);
+                Mapper.RegisterMappers(mappers);
+            });
+        }
+    }
+}
