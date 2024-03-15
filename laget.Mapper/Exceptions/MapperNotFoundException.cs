b2f448683ab89592004923616e9b82f3de061ea1
Erik Hennerfors <erik.hennerfors@obscured.se>
2024-03-15 17:51:41 +0100

    Update(d) MapperNotFoundException to print FullName instead of Name

--- laget.Mapper/Exceptions/MapperNotFoundException.cs
+++ laget.Mapper/Exceptions/MapperNotFoundException.cs
@@ -5,7 +5,7 @@ namespace laget.Mapper.Exceptions
     public class MapperNotFoundException : InvalidOperationException
     {
         public MapperNotFoundException(Type source, Type result)
-            : base($"No mappers available to perform {source.Name} -> {result.Name}")
+            : base($"No mappers available to perform {source.FullName} -> {result.FullName}")
         {
         }
     }
